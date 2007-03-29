cb2f5a6f83435094cf11aba04a351e4faaa562e0
alexrabe <alexrabe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-03-29 19:49:52 +0000

    Release v1.41
    git-svn-id: https://plugins.svn.wordpress.org/wordtube/trunk@9821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordtube.php
+++ wordtube.php
@@ -5,7 +5,7 @@ Plugin Name: wordTube
 Plugin URI: http://alexrabe.boelinger.com/?page_id=20
 Description: This plugin creates your personal YouTube plugin for wordpress. Ready for Wordpress 2.1
 Author: Alex Rabe
-Version: 1.41a
+Version: 1.41
 Author URI: http://alexrabe.boelinger.com/
 
 Copyright 2006-2007  Alex Rabe (email : alex.rabe@lycos.de)
