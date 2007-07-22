d304830b06d3ebae5dbadb9d9bc40c3dd1b39896
alexrabe <alexrabe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-07-22 07:33:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordtube/trunk@15652 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordtube-tinymce.php
+++ wordtube-tinymce.php
@@ -26,7 +26,7 @@ global $wpdb;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>NextGEN Gallery</title>
+	<title>wordTube</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<script language="javascript" type="text/javascript" src="<?php echo get_option('siteurl') ?>/wp-includes/js/tinymce/tiny_mce_popup.js"></script>
 	<script language="javascript" type="text/javascript" src="<?php echo get_option('siteurl') ?>/wp-includes/js/tinymce/utils/mctabs.js"></script>
